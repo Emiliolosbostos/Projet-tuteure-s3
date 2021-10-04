74e4548fd4114ca30849275fe7b71f58c640439c
boniver <wankertyboniver@gmail.com>
2021-10-04 21:00:25 +0200

    Connexion à MySQL sur localhost

--- pdo.php
+++ pdo.php
@@ -1,8 +1,12 @@
 <?php
 
-$user = "root";
-$pass = "";
+$user = "login";
+$pass = "mot_de_passe";
 
-$db = new PDO("mysql:host=localhost;dbname=test",$user,$pass);
-
+try {
+    $db = new PDO("mysql:host=localhost;dbname=ton_nom_de_BDD",$user,$pass); //Ne pas oublier de changer vos settings pour vous connecter à votre propre BDD et si on veut héberger le site sur un serveur il faudra renseigner l'IP
+}catch (PDOException $e){
+    print "Erreur :" . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
