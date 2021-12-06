1a55e3da5cf8ee4f15a63ec2c46833d244f22680
boniver <wankertyboniver@gmail.com>
2021-12-06 16:44:05 +0100

    Page de dactylo.php

---
+++ pages/dactylo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Dactylographie</title>
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <link href="../public/stylesheet/quiz.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php include('../includes/navbar.php'); ?>
+
+
+</body>
